61080609e92f4f0fb4bcfa8c7fd5df62b92cf400
Thomas Pouponneau <thomas.pouponneau@gmail.com>
2025-04-23 09:51:04 +0200

    Repo for 123

--- src/virtualNodes/sharing/VNodeSharingPoison.py
+++ src/virtualNodes/sharing/VNodeSharingPoison.py
@@ -27,10 +27,11 @@ class VNodeSharingPoison(VNodeSharing):
         compression_class=None,
         float_precision=None,
         attack_type='zero',
-        poison_strength=1.0,
-        poison_probability=1.0,
-        targeted_nodes=None,
-        log_poisoning_metrics=True,
+        adversarial_nodes=None,   # List of uids of adversarial nodes
+        # poison_strength=1.0,
+        # poison_probability=1.0,
+        # targeted_nodes=None,
+        # log_poisoning_metrics=True,
     ):
         """
         Constructor for poisoning class
@@ -65,32 +66,39 @@ class VNodeSharingPoison(VNodeSharing):
         )
         
         self.attack_type = attack_type
-        self.poison_strength = float(poison_strength)
-        self.poison_probability = float(poison_probability)
-        self.targeted_nodes = [] if targeted_nodes is None else targeted_nodes
-        self.log_poisoning_metrics = bool(log_poisoning_metrics)
         
-        self.poison_metrics = {
-            "rounds_poisoned": 0,
-            "total_messages": 0,
-            "poisoned_messages": 0,
-            "targeted_nodes": []
-        }
+        if isinstance(adversarial_nodes, str) and adversarial_nodes:
+            # Convert comma-separated string to list of integers
+            self.adversarial_nodes = [int(node_id.strip()) for node_id in adversarial_nodes.split(',')]
+        else:
+            self.adversarial_nodes = [] if adversarial_nodes is None else adversarial_nodes
+
+        # self.poison_strength = float(poison_strength)
+        # self.poison_probability = float(poison_probability)
+        # self.targeted_nodes = [] if targeted_nodes is None else targeted_nodes
+        # self.log_poisoning_metrics = bool(log_poisoning_metrics)
         
-        logging.info(f"Node {rank} initialized with {attack_type} poisoning attack (strength={poison_strength})")
+        # self.poison_metrics = {
+        #     "rounds_poisoned": 0,
+        #     "total_messages": 0,
+        #     "poisoned_messages": 0,
+        #     "targeted_nodes": []
+        # }
+        
+        # logging.info(f"Node {rank} initialized with {attack_type} poisoning attack (strength={poison_strength})")
     
     def _apply_poison(self, params):
         """Apply poisoning strategy to sent data (gradients)"""
         with torch.no_grad():
             if self.attack_type == 'zero':
                 params.zero_()
-            elif self.attack_type == 'flip':
-                params.mul_(-self.poison_strength)
-            elif self.attack_type == 'noise':
-                noise = torch.randn_like(params) * self.poison_strength
-                params.add_(noise)
-            elif self.attack_type == 'scale':
-                params.mul_(self.poison_strength)
+            # elif self.attack_type == 'flip':
+            #     params.mul_(-self.poison_strength)
+            # elif self.attack_type == 'noise':
+            #     noise = torch.randn_like(params) * self.poison_strength
+            #     params.add_(noise)
+            # elif self.attack_type == 'scale':
+            #     params.mul_(self.poison_strength)
         return params
     
     def get_data_to_send(self, vnodes_per_node=1, sparsity=0.0, degree=None):
@@ -100,39 +108,40 @@ class VNodeSharingPoison(VNodeSharing):
             vnodes_per_node=vnodes_per_node, sparsity=sparsity
         )
         
-        self.poison_metrics["total_messages"] += len(data_list)
+        # self.poison_metrics["total_messages"] += len(data_list)
         
         for data in data_list:
-            do_poison = np.random.random() < self.poison_probability
+            # do_poison = np.random.random() < self.poison_probability
             
-            if do_poison:
+            # if do_poison:
+            if self.uid in self.adversarial_nodes:
                 data['params'] = self._apply_poison(data['params'])
-                data['poisoned'] = True
-                self.poison_metrics["poisoned_messages"] += 1
-            else:
-                data['poisoned'] = False
+                # data['poisoned'] = True
+                # self.poison_metrics["poisoned_messages"] += 1
+            # else:
+            #     data['poisoned'] = False
                 
             data["real_node"] = self.uid
         
-        self.poison_metrics["rounds_poisoned"] += 1
+        # self.poison_metrics["rounds_poisoned"] += 1
         
-        if self.log_poisoning_metrics and self.poison_metrics["rounds_poisoned"] % 10 == 0:
-            self._log_poison_metrics()
+        # if self.log_poisoning_metrics and self.poison_metrics["rounds_poisoned"] % 10 == 0:
+        #     self._log_poison_metrics()
             
         return data_list
     
-    def _log_poison_metrics(self):
-        """Log poisoning metrics to file"""
-        metrics_path = os.path.join(self.log_dir, f"poison_metrics_{self.uid}.json")
+    # def _log_poison_metrics(self):
+    #     """Log poisoning metrics to file"""
+    #     metrics_path = os.path.join(self.log_dir, f"poison_metrics_{self.uid}.json")
         
-        if self.poison_metrics["total_messages"] > 0:
-            poison_rate = self.poison_metrics["poisoned_messages"] / self.poison_metrics["total_messages"]
-            self.poison_metrics["poison_rate"] = poison_rate
+    #     if self.poison_metrics["total_messages"] > 0:
+    #         poison_rate = self.poison_metrics["poisoned_messages"] / self.poison_metrics["total_messages"]
+    #         self.poison_metrics["poison_rate"] = poison_rate
         
-        with open(metrics_path, 'w') as f:
-            json.dump(self.poison_metrics, f, indent=2)
+    #     with open(metrics_path, 'w') as f:
+    #         json.dump(self.poison_metrics, f, indent=2)
             
-    def __del__(self):
-        """Save final metrics before destruction"""
-        if self.log_poisoning_metrics:
-            self._log_poison_metrics()
+    # def __del__(self):
+    #     """Save final metrics before destruction"""
+    #     if self.log_poisoning_metrics:
+    #         self._log_poison_metrics()
